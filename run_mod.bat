1e502444ded7b7c5c211260801545434ad3db5b4
adoral84 <nathan.andrews04@gmail.com>
2012-08-11 19:05:53 -0400

    initial commit of new repo for halflife 2 vr mode

---
+++ run_mod.bat
@@ -0,0 +1,1 @@
+@"d:\steam\steamapps\killdog84\source sdk base 2007\hl2.exe" -dev -game "d:\steam\steamapps\SourceMods\virtualhalf-life" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"d:\steam\steamapps\killdog84\sourcesdk\bin\source2007\bin\studiomdl.exe" -game "d:\steam\steamapps\SourceMods\virtualhalf-life" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
